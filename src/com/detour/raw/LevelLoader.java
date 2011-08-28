1233e103d642a71494b7c2ec43edfdd196af24a0
Clayton Langford <amplify91@gmail.com>
2011-08-28 16:09:04 -0400

    Got a working (barely working/very rough draft) level interpreter in LevelLoader class. Needs a lot of work later, but works for now.

--- src/com/detour/raw/LevelLoader.java
+++ src/com/detour/raw/LevelLoader.java
@@ -1,21 +1,38 @@
 package com.detour.raw;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 import android.content.Context;
+import android.util.Log;
 
 public class LevelLoader {
 	
 	Context mContext;
 	int program;
+	int levelWidth = 0;
+	int levelHeight = 0;
+	
+	Tile[][] tiles;
 	
 	public LevelLoader(Context c, int program, int level){
 		mContext = c;
 		this.program = program;
+		if(level==0){
+			createRandomTileMap();
+		}else{
+			createLevelFromFile(level);
+		}
 	}
 	
 	public Tile[][] getTileMap(){
-		Tile[][] tiles = new Tile[15][100];
+		return tiles;
+	}
+	
+	private void createRandomTileMap(){
+		tiles = new Tile[15][50];
 		
 		Random rand = new Random();
 		
@@ -27,8 +44,174 @@ public class LevelLoader {
 				tiles[y][x].translate(x, y);
 			}
 		}
+	}
+	
+	public void createLevelFromFile(int ln){
+		int fileId = getFileName(ln);
+		//String file;
+		//int[][] pieceInfo = null;
+		//int[][] grid = new int[1][1];
+		levelWidth = 0;
+		levelHeight = 0;
+		//int[][][] levelInfo = null;
+		String line1 = null;
+		String line4 = null;
+		String line5 = null;
+		//String line6 = null;
 		
-		return tiles;
+		try{
+			InputStream inputStream = mContext.getResources().openRawResource(fileId);
+			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream), inputStream.toString().length());
+			
+			String line = in.readLine();
+			int x = 1;
+			while (line != null) {
+				if(x==1){
+					line1 = line;
+				}else if(x==4){
+					line4 = line;
+				}else if(x==5){
+					line5 = line;
+				}/*else if(x==6){
+					line6 = read;
+				}*/
+				line = in.readLine();
+				x++;
+			}
+			
+		}catch (Exception e){
+			Log.d("ERROR-readingLevelFile", "Could not read file: " + e.getLocalizedMessage());
+		}
+		
+		if(line1!=null){
+			String[] str = line1.split("[ ]");
+			levelWidth = (int)(Double.parseDouble(str[0]));
+			levelHeight = (int)(Double.parseDouble(str[1]));
+			//grid = new int[levelHeight][levelWidth];
+			/*for(int y=0;y<levelHeight;y++){
+				for(int x=0;x<levelWidth;x++){
+					grid[y][x] = 0;
+				}
+			}*/ //Not necessary
+		}
+		
+		tiles = new Tile[levelHeight][levelWidth];
+		
+		if(line4!=null){
+			String[] str = line4.split("[ ]");
+			int[] gridArr = new int[(levelWidth*levelHeight)];
+			if(str.length!=gridArr.length){
+				Log.d("Levels", "Not correctly parsing level file. Check split() method");
+			}
+			//int tile;
+			for(int x=0;x<gridArr.length;x++){
+				gridArr[x] = (int)(Double.parseDouble(str[x]));
+			}
+			for(int x=0;x<gridArr.length;x++){
+				Tile t = createTile(gridArr[x]);
+				if(x<levelWidth){
+					t.translate(x, 0);
+					tiles[0][x] = t;
+				}else{
+					t.translate((x%levelWidth), (x/levelWidth));
+					tiles[(x/levelWidth)][(x%levelWidth)] = t;
+				}
+			}
+		}
+		
+		/*int numberOfPieces = 0;
+		if(line5!=null){
+			String[] str = line5.split("[ ]");
+			for(int x=0;x<str.length;x++){
+				if(((int)(Double.parseDouble(str[x])))!=0){
+					numberOfPieces++;
+				}
+			}
+			int[][] pieces = new int[numberOfPieces+1][3];
+			pieces[pieces.length-1] = new int[]{0};
+			
+			int currentPiece = 0;
+			for(int x=0;x<str.length;x++){
+				int pieceNum = (int)(Double.parseDouble(str[x]));
+				if(pieceNum!=0){ //TOD O try to just fix the level editor instead of this hack-ery.
+					if(pieceNum==1||pieceNum==3){
+						if(x<levelWidth){
+							pieces[currentPiece][0] = this.x[x];
+							pieces[currentPiece][1] = this.y[0-1];
+						}else{
+							pieces[currentPiece][0] = this.x[x%levelWidth];
+							pieces[currentPiece][1] = this.y[(x/levelWidth)-1];
+						}
+					}else if(pieceNum==2){
+						if(x<levelWidth){
+							pieces[currentPiece][0] = this.x[x];
+							pieces[currentPiece][1] = this.y[0-2];
+						}else{
+							pieces[currentPiece][0] = this.x[x%levelWidth];
+							pieces[currentPiece][1] = this.y[(x/levelWidth)-2];
+						}
+					}else if(pieceNum==4||pieceNum==6){
+						if(x<levelWidth){
+							pieces[currentPiece][0] = this.x[x-1];
+							pieces[currentPiece][1] = this.y[0];
+						}else{
+							pieces[currentPiece][0] = this.x[(x%levelWidth)-1];
+							pieces[currentPiece][1] = this.y[x/levelWidth];
+						}
+					}else if(pieceNum==5){
+						if(x<levelWidth){
+							pieces[currentPiece][0] = this.x[x-2];
+							pieces[currentPiece][1] = this.y[0];
+						}else{
+							pieces[currentPiece][0] = this.x[(x%levelWidth)-2];
+							pieces[currentPiece][1] = this.y[x/levelWidth];
+						}
+					}
+					
+					pieces[currentPiece][2] = pieceNum;
+					currentPiece++;
+				}
+			}
+			
+			pieceInfo = pieces;
+			setNeedsRandomized(true);
+			randomizePieceBitmaps(pieceInfo);
+		}*/
+		
+		/*if(pieceInfo!=null&&grid!=null){
+			levelInfo = new int[][][]{pieceInfo,grid};
+		}*/
+		
+		//return levelInfo;
+		
+	}
+	
+	private Tile createTile(int type) {
+		int id = 0;
+		if(type==0){
+			id = R.drawable.t9;
+		}else if(type==1){
+			id = R.drawable.t91;
+		}else if(type==2){
+			id = R.drawable.t92;
+		}else{
+			id = R.drawable.t93;
+		}
+		
+		Tile t = new Tile(mContext, GameRenderer.getScreenRatio());
+		t.setProgram(program);
+		t.loadGLTexture(id);
+		return t;
+	}
+
+	private int getFileName(int ln){
+		int levelID = 0;
+		
+		if(ln==1){
+			levelID = R.raw.rawtest1;
+		}
+		
+		return levelID;
 	}
 	
 	int getRandomPlaceholder(int x){
