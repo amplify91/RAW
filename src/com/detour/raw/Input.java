ba8e9454d7968400ffbf690f47049c98bd5c8d50
Clayton Langford <amplify91@gmail.com>
2011-10-27 21:44:08 -0400

    Found more bugs with Input, but made the situation better overall.

--- src/com/detour/raw/Input.java
+++ src/com/detour/raw/Input.java
@@ -7,10 +7,8 @@ public class Input {
 	
 	GameManager mGameManager;
 	
-	float eventX;
-	float eventY;
-	float eventX2;
-	float eventY2;
+	float[] eventX = new float[1];
+	float[] eventY = new float[1];
 	
 	int mPixelWidth = 0;
 	int mPixelHeight = 0;
@@ -24,18 +22,44 @@ public class Input {
 	
 	public boolean onTouchInput(MotionEvent event){
 		
+		/*eventX = 0.0f;
+		eventY = 0.0f;
+		eventX2 = 0.0f;
+		eventY2 = 0.0f;
+		
 		eventX = event.getX(0);
 		eventY = event.getY(0);
 		
 		eventX2 = event.getX(1);
-		eventY2 = event.getY(1);
+		eventY2 = event.getY(1);*/
 		
 		//Log.i("Touch", "x = "+eventX+" y = "+eventY+" x2 = "+eventX2+" y2 = "+eventY2);
 		//Log.i("Touch", "yMin = "+(mPixelHeight-(HUD.JUMP_BUTTON_MIN_Y*(mPixelHeight/15)))+" yMax = "+(mPixelHeight-(HUD.JUMP_BUTTON_MAX_Y*(mPixelHeight/15))));
 		
 		if(event.getAction()==MotionEvent.ACTION_DOWN){
-			if(isOnJumpButton(eventX, eventY) || isOnJumpButton(eventX2, eventY2)){
+			eventX[0] = event.getX(0);
+			eventY[0] = event.getY(0);
+			if(isOnJumpButton(eventX[0], eventY[0])){
 				jump();
+			}/*else if(isOnOtherButtons()){
+				doOtherStuff();
+			}else if(isOnSwingable()){
+				swing();
+			}*/else{
+				shoot(eventX[0], eventY[0]);
+			}
+		}else if(event.getActionMasked()==MotionEvent.ACTION_POINTER_DOWN){
+			/*eventX[event.getActionIndex()] = event.getX(event.getActionIndex());
+			Log.i("Touch", "pointer #"+event.getActionIndex());
+			eventY[event.getActionIndex()] = event.getY(event.getActionIndex());*/
+			if(isOnJumpButton(event.getX(event.getActionIndex()), event.getY(event.getActionIndex()))){
+				jump();
+			}/*else if(isOnOtherButtons()){
+				doOtherStuff();
+			}else if(isOnSwingable()){
+				swing();
+			}*/else{
+				shoot(event.getX(event.getActionIndex()), event.getY(event.getActionIndex()));
 			}
 		}else{
 			return false;
@@ -59,7 +83,7 @@ public class Input {
 	}
 	
 	private void shoot(float x, float y){
-		
+		Log.i("Touch", "Shot at "+eventX[0]+"' "+eventY[0]);
 	}
 	
 	public void setPixelDimensions(int pixelWidth, int pixelHeight){
